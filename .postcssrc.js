92ea36532663e352fab488273e8db804876fe61e
CREEATION <tcreeten@gmail.com>
2020-10-01 14:08:12 +0200

    current state

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,13 +1,13 @@
 module.exports = {
   map: false,
   plugins: [
-    require('postcss-import')({
-      from: './src/styles/creeation.css',
-    }),
+    // require('postcss-import')({
+    //   from: './src/styles/creeation.css',
+    // }),
     require('postcss-preset-env')({
       stage: 0,
     }),
-    // require('postcss-combine-media-query'),
+    require('postcss-nested'),
     require('autoprefixer'),
   ],
 }
