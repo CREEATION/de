cad09600e9fa445dcafaa061bd6c7afc0dad688c
CREEATION <tcreeten@gmail.com>
2020-12-03 04:07:56 +0100

    uses fancy new logger to show scripts count

--- gulpfile.js/tasks/scripts/index.js
+++ gulpfile.js/tasks/scripts/index.js
@@ -1,6 +1,6 @@
 "use strict"
 
-const { task_finalize } = require(`${process.cwd()}/lib`)
+const { task_finalize, utils_log } = require(`${process.cwd()}/lib`)
 
 module.exports = task_finalize(
   (cb) => {
@@ -9,6 +9,8 @@ module.exports = task_finalize(
     const rename = require("gulp-rename")
     const terser = require("gulp-terser")
     const beautifier = require("gulp-prettier")
+
+    let processed_first_script = false
 
     require("pump")(
       [
@@ -19,7 +21,33 @@ module.exports = task_finalize(
         beautifier(),
         dest("dist/assets/js"),
         terser(),
-        rename((path) => (path.extname = ".min.js")),
+        rename((path) => {
+          path.extname = ".min.js"
+
+          if (!processed_first_script) {
+            utils_log(
+              {
+                text: `>`,
+                color: "cyan",
+              },
+              {
+                text: `minifying`,
+                color: "reset",
+              },
+              {
+                text: require("globby").sync("src/scripts/*.js", { dot: true })
+                  .length,
+                color: "magenta",
+              },
+              {
+                text: `javascript files...`,
+                color: "reset",
+              }
+            )
+
+            processed_first_script = true
+          }
+        }),
         sourcemaps.write("sourcemaps"),
         dest("dist/assets/js"),
         require("browser-sync").stream({ match: "**/*.js" }),
